4313e0481574f8ff72dc29099e2e06c839ee986f
Kulbayev Yerden <47217542+PlayLife2k@users.noreply.github.com>
2019-02-13 22:16:11 +0600

    Add files via upload

---
+++ week1/Task4/ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            b = int.Parse(Console.ReadLine());
+            int[] a = new int[b];
+            for (int i = 0; i <= b; i++)
+            {
+                for (int t = 1; t <= i; t++)
+                    Console.Write("[*]");
+                Console.WriteLine();
+            }
+        }
+    }
+}
