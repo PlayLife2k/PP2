ac2b71157c09da8b59c3bec1ee5120fe3635b2b6
Kulbayev Yerden <47217542+PlayLife2k@users.noreply.github.com>
2019-02-15 04:10:14 +0600

    Update Program.cs

--- week1/Task2/ConsoleApp2/Program.cs
+++ week1/Task2/ConsoleApp2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace task2
 {
-    class Student
+    class Student//создал класс Student
     {
         public string name;
         public string id;
@@ -22,11 +22,11 @@ namespace task2
     {
         static void Main(string[] args)
         {
-            Student s = new Student();
-            s.name = Convert.ToString(Console.ReadLine());
-            s.id = Convert.ToString(Console.ReadLine());
-            s.year_of_study = Convert.ToInt32(Console.ReadLine());
-            s.PrintInfo();
+            Student s = new Student();//Обзываем класс Student буквой s
+            s.name = Convert.ToString(Console.ReadLine());//конвертируем из класса в строку и выводим в консоль
+            s.id = Convert.ToString(Console.ReadLine());//конвертируем id из класса в строку и выводим в консоль
+            s.year_of_study = Convert.ToInt32(Console.ReadLine());//конвертируем год+1 из класса в числовое значение и выводи в консоль
+            s.PrintInfo();//Вывести информацию
 
         }
     }
