3d9764fe40d9a1c9ee3ebadabbc2622e419535eb
Kulbayev Yerden <47217542+PlayLife2k@users.noreply.github.com>
2019-02-15 04:04:50 +0600

    Update Program.cs

--- week2/Task2/ConsoleApp1/Program.cs
+++ week2/Task2/ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,38 +9,30 @@ namespace ConsoleApp1
 {
     class Program
     {
-        public static bool Prime(int a)
+        public static bool Prime(int a)//создаем правила для Prime numbers
         {
-            if (a == 0 || a == 1) return false;
-            for (int i= 2; i <= Math.Sqrt(a); i++)
+            if (a == 0 || a == 1) return false;// если значение a=0 или a=1 то автоматически выдает false(не прайм)
+            for (int i= 2; i <= Math.Sqrt(a); i++)// правила для отбора prime чисел
             {
-                if (a % i == 0) return false;
+                if (a % i == 0) return false;// если a делиться на i с остатком 0 то не является Prime числом
             }
-            return true;
+            return true;// остальные значения true и являются Prime числами---->Prime(Простые)
 
         }
-        static void Main(string[] args)
+        static void Main(string[] args)//void Main--->главное правило
         {
-            FileStream fs = new FileStream(@"C:\Users\ww\source\repos\week2\Task2\input.txt", FileMode.Open, FileAccess.Read);
-            StreamReader sr = new StreamReader(fs);
-            string[] s = sr.ReadLine().Split();
-            sr.Close();
-            fs.Close();
-            FileStream fs1 = new FileStream(@"C:\Users\ww\source\repos\week2\Task2\output.txt", FileMode.Open, FileAccess.Write);
-            StreamWriter sr1 = new StreamWriter(fs1);
-            for(int i = 0; i < s.Length; i++)
+            FileStream fs = new FileStream(@"C:\Users\ww\source\repos\week2\Task2\input.txt", FileMode.Open, FileAccess.Read);//открываем и читаем файл по данному пути
+            StreamReader sr = new StreamReader(fs);//переводим иероглифы в понятный для нас язык 
+            string[] s = sr.ReadLine().Split();//Выполняет чтение строки символов из текущего потока и возвращает в виде строки , split для разделения 
+            sr.Close();//обязательно закрываем sr
+            fs.Close();//обязательно закрываем fs
+            FileStream fs1 = new FileStream(@"C:\Users\ww\source\repos\week2\Task2\output.txt", FileMode.Open, FileAccess.Write);//открываем файл но уже редактируем(НЕ ЧИТАЕМ !)
+            StreamWriter sr1 = new StreamWriter(fs1);//переводи в понятный для нас язык
+            for(int i = 0; i < s.Length; i++)//задаем размер массива
             {
-                if (Prime(int.Parse(s[i]))==true) sr1.Write(s[i] + " ");
+                if (Prime(int.Parse(s[i]))==true) sr1.Write(s[i] + " ");// тут мы используем условие которые мы писали в начале , int.Parse---->преобразуе строку s в математический массик с размером ш---->s[i], если выполняется условие простых чисел то эти числа переносятся в новый файл output с пробелом
             }
-            sr1.Close();
-
-
-
-
-
-
-
-
+            sr1.Close();//закрываем StreamReader1
         }
     }
 }
